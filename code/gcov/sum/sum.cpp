479eeca71be443b555c0058d73cfa429cc424499
Kuien Liu <kuien@iscas.ac.cn>
2017-03-13 11:53:47 +0800

    able to fractional coverage report with diff-cover
    command:
    	make clean coverage genxml diffcover genhtml
    
    Diff coverage is the percentage of new or modified lines that are
    covered by tests. This provides a clear and achievable standard for
    code review: If you touch a line of code, that line should be covered.
    Code coverage is every developer's responsibility!
    
    ref: http://diff-cover.readthedocs.io/en/latest/

--- code/gcov/sum/sum.cpp
+++ code/gcov/sum/sum.cpp
@@ -1,11 +1,16 @@
 #ifndef SUM_FUNC_CPP
 #define SUM_FUNC_CPP
+#include <stdio.h>
 
 unsigned int sum(unsigned int n) {
     unsigned int i, total = 0;
 
     for (i = 0; i < n; i++) {
         total += i;
+    }
+
+    if (total == 924) {
+        printf("It is my lucky number.\n");
     }
 
     return total;
