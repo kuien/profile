74e7dfa5f220ad63faa3c256b8ef4ea8a85978a5
Kuien Liu <kuien@iscas.ac.cn>
2017-04-13 20:42:51 +0800

    add another individual function without UT

---
+++ code/gcov/sum/max.cpp
@@ -0,0 +1,13 @@
+#ifndef MAX_FUNC_CPP
+#define MAX_FUNC_CPP
+#include <stdio.h>
+
+unsigned int max(unsigned int a, unsigned int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+#endif
